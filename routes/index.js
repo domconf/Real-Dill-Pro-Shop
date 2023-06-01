ab1a28d1cdd41b247ed5899dad71ac9a54d44f01
antpica <antpica24@yahoo.com>
2023-05-31 21:32:56 -0400

    auth0 login/logout & navbar

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { requiresAuth } = require('express-openid-connect');
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Auth0 Webapp sample Nodejs',
+    isAuthenticated: req.oidc.isAuthenticated(),
+    user: req.oidc.user
+  });
+});
+
+router.get('/profile', requiresAuth(), function (req, res, next) {
+  res.render('profile', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Profile page',
+    isAuthenticated: req.oidc.isAuthenticated(),
+    user: req.oidc.user
+  });
+});
+
+module.exports = router;
